7881e82a68ca7c0d62d44739740abb25f9b1e9d0
KseniyaAnikin <100955276+KseniyaAnikin@users.noreply.github.com>
2023-02-01 21:01:58 +0300

    work with modal

--- src/modal.js
+++ src/modal.js
@@ -1,26 +1,37 @@
 import './style.css';
 import {el, setChildren} from 'redom';
 
-export const overlay = el('div');
-overlay.classList.add('overlay');
-export const modal = el('div');
-modal.classList.add('modal');
+export const overlay = el('div', { class: 'overlay' });
+export const modal = el('div', { class: 'modal' });
+const title = el('h2', 'Новый клиент', { class: 'modal-title' });
+const form = el('form', { class: 'modal-form' });
 
-const title = el('h2', 'Изменить данные');
-title.classList.add('modal-title');
+const containerLastName = el('div', { class: 'modal-inputcontainer' });
+const lastName = el('input', { type: 'text', class: 'modal-input'});
+const labelLastName = el('label', 'Фамилия', { class: 'modal__placeholder'}, el('span', '*',{ class: 'pl-span'}));
 
-const form = el('form');
-form.classList.add('modal-form');
-const lastName = el('input', { type: 'text'});
-lastName.classList.add('modal-input');
-const firstName = el('input', { type: 'text'});
-firstName.classList.add('modal-input')
-const patronymic = el('input', { type: 'text'});
-patronymic.classList.add('modal-input')
+const containerName = el('div', { class: 'modal-inputcontainer' });
+const name = el('input', { type: 'text', class: 'modal-input'});
+const labelName = el('label', 'Имя', { class: 'modal__placeholder'}, el('span', '*',{ class: 'pl-span'}));
 
+const containerPatronymic = el('div', { class: 'modal-inputcontainer' });
+const patronymic = el('input', { type: 'text', class: 'modal-input'});
+const labelPatronymic = el('label', 'Отчество', { class: 'modal__placeholder'});
 
-setChildren(form, [ title, lastName, firstName, patronymic])
-setChildren(modal, form)
+setChildren(containerPatronymic, [patronymic, labelPatronymic]);
+setChildren(containerName, [name,labelName]);
+setChildren( containerLastName, [lastName,labelLastName]);
+setChildren(form, [title, containerLastName, containerName, containerPatronymic]);
+
+const addContainer = el('div', { class: 'modal-addcontainer'});
+const addContactBtn = el('button', 'Добавить контакт', { class: 'modal-addbtn'});
+
+const saveBtn = el('button', 'Сохранить', { class: 'modal-savebtn' });
+const cancellationBtn = el('button', 'Отмена' , { class: 'modal-cancel' });
+const closeModalBtn = el('button', { class: 'modal-closebtn'})
+
+setChildren(addContainer, addContactBtn);
+setChildren(modal, [form, addContainer, saveBtn, cancellationBtn, closeModalBtn]);
 
 
 
