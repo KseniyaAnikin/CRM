ee24cb9fdb037d12e6f40037b1daac610707c27f
KseniyaAnikin <100955276+KseniyaAnikin@users.noreply.github.com>
2023-03-06 19:41:25 +0300

    additions

--- scripts/index.js
+++ scripts/index.js
@@ -2,12 +2,12 @@ import { modal } from "./modal.js";
 import { getContactsInfo } from "./data.js";
 
 const wrapper = document.querySelector('.wrapper');
-const main = document.querySelector('.main');
+// const main = document.querySelector('.main');
 const container = document.createElement('div');
 container.classList.add('container', 'main__container');
-main.classList.add('main');
+// main.classList.add('main');
 
-const createHeader = (el) =>{
+const createHeader = (el) => {
   const header = document.querySelector('.header');
   const headerContainer = document.createElement('div');
   const logo = document.createElement('div');
@@ -33,6 +33,52 @@ const createHeader = (el) =>{
   el.append(header)
 };
 
+const createTable = async(el) => {
+  const main = document.querySelector('.main');
+  main.classList.add('main');
+  const header = document.createElement('h1');
+  header.textContent = 'Клиенты';
+  header.classList.add('main-header');
+  main.append(header);
+  el.append(main);
+
+  const table = document.createElement('table');
+  table.innerHTML = 
+  `<tr>
+    <th>ID</th>
+    <th>Фамилия Имя Отчество</th>
+    <th>Дата и время создания</th>
+    <th>Последние изменения</th>
+    <th>Контакты</th>
+    <th>Действия</th>
+  </tr>`
+
+  const spiner = document.createElement('div');
+  spiner.innerHTML = `<div class="d-flex justify-content-center">
+    <div class="spinner-border" role="status">
+    </div>
+  </div>`
+
+  table.append(spiner)
+  
+
+  main.append(table);
+
+  const data = await getContactsInfo();
+  data.forEach(element => {
+      const row = document.createElement('tr');
+      row.classList.add('table-row')
+      row.innerHTML = `<th>${element.id}</th>
+        <th>${element.surname} ${element.name} ${element.lastName} </th>
+        <th>${element.createdAt}</th>
+        <th>${element.updatedAt}</th>
+        <th>Изменить</th>
+        <th>Удалить</th>`
+      table.append(row)
+    });
+
+}
+
 const createAddButton = (el) => {
   const button = document.createElement('button');
   const buttonContainer = document.createElement('div');
@@ -49,14 +95,6 @@ const createAddButton = (el) => {
 };
 
 createHeader(wrapper);
+createTable(wrapper)
 createAddButton(wrapper);
 
-
-//test get contacts
-// const data = await getContactsInfo()
-
-// data.forEach(element => {
-//   const table = document.createElement('div')
-//   table.innerHTML = element.name
-//   wrapper.append(table)
-// });
